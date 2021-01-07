994cc7f69d0c5a41d6c04e49b84a22523474d70a
Alexander Emashev <alex.emashev@gmail.com>
2021-01-07 23:38:04 +0300

    Added plane states

--- src/plane.ts
+++ src/plane.ts
@@ -5,14 +5,21 @@ const PLANE_SETTINGS = {
   spriteFrameLimit: 5
 };
 
+enum planeStates {
+  flyDown = 'flyDown',
+  flyUp = 'flyUp',
+  crash = 'crash'
+}
+
 /**
  * Class for plane sprite
  */
 export class Plane {
 
+  planeState = planeStates.flyUp;
   private spriteNumber = 0;
   private currentSpriteFrame = 0;
-  private sprites = [
+  private flyDownSprites = [
     {
       sx: -3.4,
       sy: 0,
@@ -43,6 +50,39 @@ export class Plane {
       dWidth: 100,
       dHeight: 100
     },
+  ];
+  // ToDo: fix clipping (increase overall sWidth and sHeight)
+  private flyUpSprites = [
+    {
+      sx: 259,
+      sy: 0,
+      sWidth: 80,
+      sHeight: 80,
+      dx: 100,
+      dy: 220,
+      dWidth: 100,
+      dHeight: 100
+    },
+    {
+      sx: 344.9,
+      sy: 0,
+      sWidth: 80,
+      sHeight: 80,
+      dx: 100,
+      dy: 220,
+      dWidth: 100,
+      dHeight: 100
+    },
+    {
+      sx: 442.4,
+      sy: 0,
+      sWidth: 80,
+      sHeight: 80,
+      dx: 100,
+      dy: 220,
+      dWidth: 100,
+      dHeight: 100
+    },
   ]
 
   constructor() {
@@ -55,13 +95,29 @@ export class Plane {
   draw(context: CanvasRenderingContext2D) {
     let spriteNumber = this.getSpriteNumber();
 
-    context.drawImage(
-      gameResources.spritePlane,
-      this.sprites[spriteNumber].sx, this.sprites[spriteNumber].sy, // top-left corner of the source
-      this.sprites[spriteNumber].sWidth, this.sprites[spriteNumber].sHeight, // width and height of the sprite in the source
-      this.sprites[spriteNumber].dx, this.sprites[spriteNumber].dy, // location on the canvas
-      this.sprites[spriteNumber].dWidth, this.sprites[spriteNumber].dHeight // width and height on the canvas
-    );
+    switch (this.planeState) {
+      case planeStates.flyDown:
+        context.drawImage(
+          gameResources.spritePlane,
+          this.flyDownSprites[spriteNumber].sx, this.flyDownSprites[spriteNumber].sy, // top-left corner of the source
+          this.flyDownSprites[spriteNumber].sWidth, this.flyDownSprites[spriteNumber].sHeight, // width and height of the sprite in the source
+          this.flyDownSprites[spriteNumber].dx, this.flyDownSprites[spriteNumber].dy, // location on the canvas
+          this.flyDownSprites[spriteNumber].dWidth, this.flyDownSprites[spriteNumber].dHeight // width and height on the canvas
+        );
+        break;
+      case planeStates.flyUp:
+        context.drawImage(
+          gameResources.spritePlane,
+          this.flyUpSprites[spriteNumber].sx, this.flyUpSprites[spriteNumber].sy, // top-left corner of the source
+          this.flyUpSprites[spriteNumber].sWidth, this.flyUpSprites[spriteNumber].sHeight, // width and height of the sprite in the source
+          this.flyUpSprites[spriteNumber].dx, this.flyUpSprites[spriteNumber].dy, // location on the canvas
+          this.flyUpSprites[spriteNumber].dWidth, this.flyUpSprites[spriteNumber].dHeight // width and height on the canvas
+        );
+        break;
+      case planeStates.crash:
+        // ToDo: plane crash state
+        break;
+    }
   }
 
 
